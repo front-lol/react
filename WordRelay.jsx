ca26b17b353ecb137c46d3f1505ec4eac1923f83
junhyeok <wnsgur1569@gmail.com>
2021-05-01 10:58:36 +0900

    basic

---
+++ WordRelay.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+const {Component} = React
+
+class WordRelay extends React.Component{
+    state={
+
+    }
+
+    render(){
+        
+    }
+}
+
+module.exports = WordRelay
